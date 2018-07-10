70f029a8ccd6574c1cdebfc53e9b21737c65f80d
Datseris <datseris.george@gmail.com>
2018-07-10 23:24:21 +0200

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -7,6 +7,6 @@ deploydocs(
     deps   = Deps.pip("Tornado>=4.0.0,<5.0.0", "mkdocs",
     "mkdocs-material" ,"python-markdown-math", "pygments", "pymdown-extensions"),
     repo   = "github.com/JuliaMusic/JuliaMusic_documentation.jl.git",
-    julia  = "0.6",
+    julia  = "nightly",
     osname = "linux"
 )
