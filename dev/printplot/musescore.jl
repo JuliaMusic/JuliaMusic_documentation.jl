828986b0fd41f8daf00d8d8ed59092d0ecd53fa4
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-16 16:16:35 +0000

    build based on 0061fe0

--- dev/printplot/musescore.jl
+++ dev/printplot/musescore.jl
@@ -18,7 +18,8 @@
 
 # ## Creating a Score out of some Notes
 
-using MusicManipulations
+using MusicManipulations # tools for manipulating notes in Julia
+using MusicVisualizations # tools for visualizing these notes
 cd(@__DIR__) #src
 
 # We first load the test MIDI file "Doxy".
