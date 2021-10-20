32808b2317b0d52c8c0ab0dc2bfd32f88e062dd6
Documenter.jl <documenter@juliadocs.github.io>
2021-10-20 14:02:12 +0000

    build based on 448aaff

---
+++ previews/PR14/blog/garibaldi_dragadiddle.jl
@@ -0,0 +1,172 @@
+# # Garibaldi's Foot+Hand doubles
+#
+# Have a look at the following simple rudiment involving a double stroke
+#
+# ![dragadiddle](draga_example_1.PNG)
+#
+# Here the sticking could be single strokes, as in **Rr L R L** on repeat, or
+# it can also follow the standard "dragadiddle", **Rr L R R Ll R L L**.
+#
+# David Garibaldi uses and teaches an interesting concept that replaces the
+# double stroke by a foot+hand combination. This works irrespectively of the
+# above stickings, and the double stroke bomes a foot stroke and a tom stroke.
+# It looks like this:
+#
+# ![dragadiddle_garibalid](draga_example_2.PNG)
+#
+# I wished to study this concept in detail, and in the following I set up
+# some code that produces exercises for me to work on. It also
+# uses a relatively new feature of [`random_notes_sequence`](@ref) which
+# can weight different patterns with different probabilities, so that they appear
+# more or less frequently.
+
+# ## Textbook exercises
+# Before we devle into the actual random sequences of this new kind of pattern
+# I create some straight forward exercises that have this pattern in different
+# locations in the bar. For the sticking, I assume the single strokes, so that
+# I also have exercise variants with two foot-hand strokes next to each other.
+#
+# First, let's define the basic variables necessary for the code.
+# Be sure that you have read the section on [Drum Notation](@ref) before reading this,
+# to be able to understand how I actually write drum notation in MuseScore.
+
+
+using MusicVisualizations # re-exports MusicManipulations
+cd(@__DIR__) #src
+
+bass = "Acoustic Bass Drum"
+snare = "Acoustic Snare"
+tom = "Low-Mid Tom"
+tpq = 960 # duration of a quarter note in ticks
+sixt = 960 ÷ 4 # duration of a sixteenth note in ticks
+
+# For creating the patterns, one should notice that only two different
+# "motifs" are necessary, one with the foot+hand double stroke and one
+# that is only a single stroke. We can then use clever programming to create
+# all possible combinations.
+
+motif0 = [DrumNote(snare, 0, sixt)]
+
+motif1 = [DrumNote(bass, 0, sixt÷2), DrumNote(tom, sixt÷2, sixt÷2)]
+
+# %% Generate single exercises #src
+# The first step is to create some basic exercises that put this double foot+hand
+# stroke into the different possible locations within a quarter note.
+# These variations are 4 in total. In the following we will store them
+# all into a single `exercises` container, while each exercise will be
+# `bars` bars long.
+
+bars = 2
+exercises = Note[]
+
+for j in 1:4 # create the 4 possible variations
+
+    ## Create 1 quarter note of the variations
+    exercise = [translate(motif0, k*sixt) for k in 0:3]
+    exercise[j] = translate(motif1, (j-1)*sixt)
+    exercise = vcat(exercise...)
+    ex0 = copy(exercise)
+
+    ## Repeat to fill one bar with variation. 1 quarter note = 4sixt
+    for k in 1:3; append!(exercise, translate(ex0, 4k*sixt)); end
+    ex0 = copy(exercise)
+
+    ## Repeat as many bars as needed. 1 bar = 16 sixt
+    for b in 0:bars-1; append!(exercise, translate(ex0, 16b*sixt)); end
+
+    ## Add it to the exercises (but translated for the bars)
+    append!(exercises, translate(exercise, (j-1)*bars*16*sixt))
+end
+
+# %% Generate double exercises #src
+# We then create a second exrcise type where one quarter note will contain
+# two doubles of foot+hand. There are two ways for this (assuming that we do *not*
+# want the doubles to be next to each other).
+
+for j in 5:6
+    exercise = [translate(motif0, k*sixt) for k in 0:7] # repeat it 8 times
+    for m in (j, j+2)
+        exercise[m] = translate(motif1, (m-1)*sixt)
+    end
+    exercise = vcat(exercise...)
+    ex0 = copy(exercise)
+
+    ## Repeat to fill one bar with variation. 1 quarter note = 4sixt
+    ## Notice this is a 2 quarter not pattern
+    for k in 1:1; append!(exercise, translate(ex0, 8k*sixt)); end
+    ex0 = copy(exercise)
+
+    ## Repeat as many bars as needed. 1 bar = 16 sixt
+    for b in 0:bars-1; append!(exercise, translate(ex0, 16b*sixt)); end
+
+    ## Add it to the exercises (but translated for the bars)
+    append!(exercises, translate(exercise, (j-1)*bars*16*sixt))
+end
+
+# Finally, one last exercise with the foot+hand double repeated
+# every three sixteen-th notes.
+
+motif001 = [
+DrumNote(bass, 0, sixt÷2),
+DrumNote(tom, sixt÷2, sixt÷2),
+DrumNote(snare, sixt, sixt),
+DrumNote(snare,2sixt, sixt),
+]
+
+j = 7
+exercise = [translate(motif001, 3k*sixt) for k in 0:7] # repeat it 10 times
+exercise = vcat(exercise...)
+append!(exercises, translate(exercise, (j-1)*bars*16*sixt))
+exercises
+
+# Nowe we use [`musescore`](@ref) to make a sheet of the exercises
+musescore("garibaldi_draga_exercise.png", Notes(exercises, tpq)) #src
+# ```julia
+# musescore("garibaldi_draga_exercise.png", Notes(exercises, tpq))
+# ```
+
+# ![exercises](garibaldi_draga_exercise-1.png)
+
+# ## Randomized exercises
+# %% Generate simple random sequence of this thing! #src
+
+# Now I want to have random sequences with a foot+hand double stroke springled
+# into random locations. This is easy to do with[`random_notes_sequence`](@ref).
+# However, I want to be sure that a double stroke will never be followed by
+# another double stroke. To ensure this, I define
+
+motif2 = [
+DrumNote(bass, 0, sixt÷2),
+DrumNote(tom, sixt÷2, sixt÷2),
+DrumNote(snare, sixt, sixt),
+]
+
+# Recall that `motif0` is a normal single stroke. Therefore, I have to randomly
+# mix `motif0` and `motif2`
+
+motifs = Notes.([motif0, motif2], tpq)
+
+# To make the exercise easy, I want the double stroke to not appear too often.
+# This is easily done by adjusting the  `weight` keyword:
+
+rseq, = random_notes_sequence(motifs, 16sixt*16; weights = [4, 1])
+rseq
+
+# And again, export it into a score:
+
+musescore("garibaldi_draga_sequence_easy.png", rseq) #src
+# ```julia
+# musescore("garibaldi_draga_sequence_easy.png", Notes(exercises, tpq))
+# ```
+# ![exercises](garibaldi_draga_sequence_easy-1.png)
+
+# %% Generate harder random sequence #src
+# Finally, I'll make one more exercise where the foot+hand double appears a bit
+# more frequently.
+
+rseq, = random_notes_sequence(motifs, 16sixt*16; weights = [2, 1])
+musescore("garibaldi_draga_sequence_hard.png", rseq) #src
+# ```julia
+# musescore("garibaldi_draga_sequence_hard.png", Notes(exercises, tpq))
+# ```
+# ![exercises](garibaldi_draga_sequence_hard-1.png)
---
+++ previews/PR14/printplot/musescore.jl
@@ -0,0 +1,113 @@
+# # Printing into a Score using MuseScore
+
+# [MuseScore](https://musescore.org) is a wonderful and open source _professional_
+# music score editor with a huge amount of capabilities. `MusicManipulations`
+# provides a convenient interface that can instantly print any [`Notes`](@ref)
+# or [`MIDIFile`](@ref) structure via MuseScore.
+
+#md # ```@docs
+#md # musescore
+#md # ```
+
+# !!! tip "Quantize your notes!"
+#     Keep in mind that the score creation capabilities of MuseScore rely upon
+#     having well-defined notes. This means that you should use the function
+#     [`quantize`](@ref) to quantize both the position and duration of your notes!
+
+
+
+# ## Creating a Score out of some Notes
+
+using FileIO # used for loading the midi files
+using MusicManipulations # tools for manipulating notes in Julia
+using MusicVisualizations # tools for visualizing these notes
+cd(@__DIR__) #src
+
+# We first load the test MIDI file "Doxy".
+# The third track has the notes of the Bass:
+midi = load(testmidi()) # read the "test" Doxy MIDI recording.
+bass = getnotes(midi, 3)
+basstrim = bass[1:50]
+# Because the notes of the Bass are already quantized we can already
+# print them into a staff using MuseScore:
+
+musescore("bass.png", basstrim) #src
+# ```julia
+# musescore("bass.png", basstrim)
+# ```
+
+# ![Bass score](bass-1.png)
+
+# Amazingly MuseScore deduces automatically the clef and even the key of
+# the piece!
+
+# ## Creating a full Score out of a MIDI file
+# You can also pass a full MIDI file to [`musescore`](@ref).
+
+piano = getnotes(midi, 4)
+
+# However, MuseScore has decent results only with quantized notes.
+# Let's quantize on a triplet grid using [`quantize`](@ref).
+qpiano = quantize(piano, [0, 1//3, 2//3, 1])
+
+# and save both tracks into a midi file:
+ptrack = MIDITrack()
+addnotes!(ptrack, qpiano)
+addtrackname!(ptrack, "Doxy")
+smidi = MIDIFile(1, 960, [midi.tracks[3], ptrack])
+
+# and then save the full thing as `.pdf` or `.png`:
+musescore("doxy.png", smidi) #src
+
+#md # ```julia
+#md # musescore("doxy.pdf", smidi)
+#md # ```
+
+# The first page looks like this:
+# ![Full score](doxy-1.png)
+
+# When given multiple tracks MuseScore displays the name of the track ([`trackname`](@ref)),
+# as well as the instrument it automatically chose to represent it.
+
+# ## Drum Notation
+# It is also possible to use MuseScore to create drum notation.
+# The process for this is almost identical with the above, with two differences.
+# Firstly, the pitch of each note must have a specific value that maps
+# the the actual drum instrument, and secondly all notes must be written on channel `9`.
+
+# The function [`DrumNote`](@ref) simplifies this process:
+
+# ```@docs
+# DrumNote
+# ```
+
+# And this is the drum key we use:
+
+DRUMKEY
+
+# Here is an example where we create the "basic rock groove" in drum notation:
+
+tpq = 960; e = 960÷2 # eigth note = quarter note ÷ 2
+bass = "Acoustic Bass Drum"
+snare = "Acoustic Snare"
+hihat = "Closed Hi-Hat"
+
+# We make the 8 hihat notes
+rock = [DrumNote(hihat, i*e, e) for i in 0:7]
+
+# add 2 bass drums
+push!(rock, DrumNote(bass, 0, e), DrumNote(bass, 4e, e))
+
+# and add 2 snare drums
+push!(rock, DrumNote(snare, 2e, e), DrumNote(snare, 6e, e))
+
+# and finally bundle the notes with the ticks per quarter note
+rock = Notes(rock, tpq)
+
+# and then call MuseScore to actually make the score
+cd(@__DIR__) #src
+musescore("rock.png", rock) #src
+# ```julia
+# musescore("rock.png", rock)
+# ```
+# ![](rock-1.png)
